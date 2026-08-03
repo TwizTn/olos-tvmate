aa5a00445ff637b16bfe154afa9743a659f31455
TwizTn <157879755+TwizTn@users.noreply.github.com>
2026-08-03 15:26:27 +0200

    Update tvmate.py

--- tvmate.py
+++ tvmate.py
@@ -83,7 +83,7 @@ CONFIG_PATH = os.path.join(app_dir(), "config.json")
 PORT = 777
 
 # --- versioning & auto-update ---
-VERSION = "0.777.b15"
+VERSION = "0.777.b20"
 
 BANNER = r'''
   ___  _        _     _______     ____  __      __ 
